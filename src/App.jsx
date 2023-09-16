e04df4676d4dd30a731b0089291f2a0ead068568
Yasir L <yasira@sharepointxperts.com>
2023-09-17 00:06:14 +0500

    intial setup with primereact

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+//core
+import "primereact/resources/primereact.min.css"; 
+import { Button } from 'primereact/button';
+
+function App() {
+
+  return (
+    <>
+       <Button label="Check" icon="pi pi-check" />
+    </>
+  )
+}
+
+export default App
